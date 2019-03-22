58d17b81ed1b17de5a4698b20ccebe4a5101fe60
kvannamalai <kvannamalai@gmail.com>
2019-03-21 23:38:24 -0700

    D

---
+++ XLRelease/Samples & Tutorials/Github.groovy
@@ -0,0 +1,23 @@
+// Exported from:        http://lp-PC:5516/#/templates/FolderSamplesAndTutorials-Releasefbf0f1e62fbf4932a1fdbe2d991bf797/releasefile
+// XL Release version:   8.5.4
+// Date created:         Thu Mar 21 12:07:17 PDT 2019
+
+xlr {
+  template('Github') {
+    folder('Samples & Tutorials')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-03-21T09:00:00-0700')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('day1') {
+            
+          }
+          manual('d') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
