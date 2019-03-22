28c01be0ef07b419cb94feb1ad92f0ff1926a331
kvannamalai <kvannamalai@gmail.com>
2019-03-21 23:39:46 -0700

    k

---
+++ XLRelease/Samples & Tutorials/aksar1.groovy
@@ -0,0 +1,23 @@
+// Exported from:        http://lp-PC:5516/#/templates/FolderSamplesAndTutorials-Release849c6293ed3a482cbfe5c217acad5079/releasefile
+// XL Release version:   8.5.4
+// Date created:         Thu Mar 21 12:08:39 PDT 2019
+
+xlr {
+  template('aksar1') {
+    folder('Samples & Tutorials')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-03-21T20:30:00-0700')
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('Midleware') {
+            
+          }
+          manual('frpus') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
